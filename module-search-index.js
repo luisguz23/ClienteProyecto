6be7e0b9c65ccf9ba2b2b5eb084768e812799224
luisg <luguzman@estudiantec.cr>
2022-11-01 00:25:04 -0600

    ultimos detalles y javadoc x3

---
+++ module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"com.example.clienteproyecto"}];updateSearchResults();
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.clienteproyecto","l":"Conexion"},{"p":"com.example.clienteproyecto","l":"Main"}];updateSearchResults();
